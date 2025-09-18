dcd0f968e25700ba0d53046da520c42edd55020a
unknown <you@example.com>
2025-09-18 14:45:58 +0300

    add migrationapp

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Amazon.Data;
+using Amazon.Domain;
+using System.Text.Json;
+
+namespace MigrationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadDefaultEmployeesData();
+        }
+
+        private static void LoadDefaultEmployeesData()
+        {
+            string contentAsString = File.ReadAllText("Data.json");
+
+            var employees = JsonSerializer.Deserialize<List<Employee>>(contentAsString);
+
+            using(var constext = new AmazonContext())
+            {
+                constext.Employees.AddRange(employees);
+                constext.SaveChanges();
+            }
+        }
+    }
+}
